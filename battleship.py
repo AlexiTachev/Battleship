4a1baf15215d29ac198aebc444a543d267a8cb36
Alexi Tachev <alexi.tachev@gmail.com>
2015-07-05 14:24:55 -0400

    The Display of the board.

--- battleship.py
+++ battleship.py
@@ -13,3 +13,15 @@ class Board:
             for j in range(5):
                 row += "#" if random() < 0.2 else "~"
             self.board.append(row)
+
+    def display(self):
+        ''' Prints the game board to the screen. Replaces any ships with water so
+        the opponent cannot see them when guessing. '''
+        for i in range(5):
+            row = ""
+            for j in range(5):
+                if self.board[i][j] == "#":
+                    row += "~"
+                else:
+                    row += self.board[i][j]
+            print row
